a5bc9dacfca83fa0e8a4a476f856b45260204449
あみ <music.amiami@gmail.com>
2021-12-06 10:40:44 +0900

    1206

---
+++ test/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+
+$dsn='mysql:host=mysql154.phy.lolipop.lan;
+  dbname=LAA1290560-blue;charset=utf8';
+  $username='LAA1290560';
+  $password='green';
+  $pdo=new PDO($dsn,$username,$password);
+  $sql=$pdo->prepare("select * from merchandise where id=?");
+        $sql->execute(['0']);
+        foreach($sql as $row){
+          // ↓これだけではただURLが表示されるだけ
+          echo $row['merchandise_img'];
+          // イメージタグを使う必要がある
+          echo '<img src="',$row['merchandise_img'],'">';
+
+    }
+?>
+
+
+</body>
+</html>
